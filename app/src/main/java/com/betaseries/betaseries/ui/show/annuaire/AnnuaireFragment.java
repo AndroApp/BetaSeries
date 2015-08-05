b8f313d075c2f127af266c5db1caa13e02c159cc
Kevin De Jesus Ferreira <kevindejesusferreira@gmail.com>
2015-08-05 18:17:48 +0200

    Rajout du fragment annuaire

---
+++ app/src/main/java/com/betaseries/betaseries/ui/show/annuaire/AnnuaireFragment.java
@@ -0,0 +1,59 @@
+package com.betaseries.betaseries.ui.show.annuaire;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.betaseries.betaseries.R;
+import com.betaseries.betaseries.model.Show;
+import com.betaseries.betaseries.ui.AbstractFragment;
+import com.betaseries.betaseries.ui.show.ShowDetailActivity;
+import com.betaseries.betaseries.webservice.Constants;
+import com.github.florent37.carpaccio.Carpaccio;
+import com.github.florent37.carpaccio.CarpaccioLogger;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import rx.android.schedulers.AndroidSchedulers;
+
+/**
+ * Created by florentchampigny on 03/08/15.
+ */
+public class AnnuaireFragment extends AbstractFragment {
+
+    public static AnnuaireFragment newInstance() {
+        return new AnnuaireFragment();
+    }
+
+    @Bind(R.id.carpaccio)
+    Carpaccio carpaccio;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_unseen_episodes,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+
+        CarpaccioLogger.ENABLE_LOG = true;
+
+        betaSeriesAPI.serieListeSeries(Constants.ORDER_FOLLOWERS, -1)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(betaSerieResponse -> {
+                    carpaccio.mapList("show", betaSerieResponse.getShows());
+                });
+
+        carpaccio.onItemClick("show", (show, i, view1) -> {
+            Intent intent = new Intent(getActivity(), ShowDetailActivity.class);
+            intent.putExtra(ShowDetailActivity.SHOW, (Show)show);
+            startActivity(intent);
+        });
+    }
+}
