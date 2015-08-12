e97af2d6b982dd02ffc795bf9e041f294b3ed540
Florent <champigny.florent@gmail.com>
2015-08-12 23:13:25 +0200

    added light detail episode

--- app/src/main/java/com/betaseries/betaseries/ui/episodes/detail/EpisodedetailFragment.java
+++ app/src/main/java/com/betaseries/betaseries/ui/episodes/detail/EpisodedetailFragment.java
@@ -54,7 +54,7 @@ public class EpisodeDetailFragment extends AbstractFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_episode_detail, container, false);
+        return inflater.inflate(R.layout.fragment_episode_detail_light, container, false);
     }
 
     @Override
