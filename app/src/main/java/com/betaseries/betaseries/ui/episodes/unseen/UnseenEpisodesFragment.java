4a7a65ef1242ad197e0dc18093e8f86229d76f5e
Florent <champigny.florent@gmail.com>
2015-08-14 16:40:18 +0200

    black background images

--- app/src/main/java/com/betaseries/betaseries/ui/episodes/unseen/UnseenEpisodesFragment.java
+++ app/src/main/java/com/betaseries/betaseries/ui/episodes/unseen/UnseenEpisodesFragment.java
@@ -24,7 +24,6 @@ import com.github.florent37.carpaccio.controllers.adapter.RecyclerViewCallbackAd
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 /**
